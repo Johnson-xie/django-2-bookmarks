639a9269185442be8a76a7d3acfb96fdd66645f3
Johnson-xie <johnson19921005@163.com>
2019-10-01 10:23:09 +0800

    login and logout

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    # path('login/', views.user_login, name='login'),
+
+    # 该视图返回了一个next参数，用于登录成功之后的跳转
+    path('login/', auth_views.LoginView.as_view(), name='login'),  # 尾部是什么再按一次可以跳出
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
+]
